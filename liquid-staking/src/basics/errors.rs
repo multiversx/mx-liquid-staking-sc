ebd16a753851bb63ce8f0a758e227b851d03a6f4
Alin Cruceat <alyn_509@yahoo.com>
2024-11-28 11:51:36 +0200

    remove unused errors

--- liquid-staking/src/basics/errors.rs
+++ liquid-staking/src/basics/errors.rs
@@ -1,11 +1,9 @@
-//pub static ERROR_ACTIVE: &[u8] = b"Active state";
 pub static ERROR_NOT_ACTIVE: &[u8] = b"Not active";
 pub static ERROR_LS_TOKEN_NOT_ISSUED: &[u8] = b"LS token not issued";
 pub static ERROR_DELEGATION_CONTRACT_NOT_INITIALIZED: &[u8] =
     b"Delegation contract was not initialized yet";
 pub static ERROR_INSUFFICIENT_GAS: &[u8] = b"Insufficient gas remaining for the callback";
 
-//pub static ERROR_CLAIM_START: &[u8] = b"Claim operation must be new or pending";
 pub static ERROR_CLAIM_IN_PROGRESS: &[u8] = b"Claim operation is already in progress";
 pub static ERROR_OLD_CLAIM_START: &[u8] =
     b"Previous claimed rewards must be redelegated or lesser than 1 EGLD";
@@ -19,12 +17,8 @@ pub static ERROR_BAD_PAYMENT_TOKEN: &[u8] = b"Bad payment token";
 pub static ERROR_BAD_PAYMENT_AMOUNT: &[u8] = b"Insufficient delegated amount";
 pub static ERROR_BAD_WHITELIST_FEE: &[u8] = b"Whitelisting contracts costs 1 EGLD";
 pub static ERROR_INSUFFICIENT_UNSTAKE_AMOUNT: &[u8] = b"Insufficient unstake amount";
-//pub static ERROR_INSUFFICIENT_UNBONDED_AMOUNT: &[u8] = b"Insufficient incoming withdraw amount";
 pub static ERROR_INSUFFICIENT_LIQUIDITY: &[u8] = b"Insufficient liquidity minted";
 pub static ERROR_INSUFFICIENT_LIQ_BURNED: &[u8] = b"Insufficient liquidity burned";
-
-//pub static ERROR_NOT_ENOUGH_RESERVE: &[u8] = b"Not enough reserve";
-//pub static ERROR_NOT_ENOUGH_LP: &[u8] = b"Not enough LP token supply";
 
 pub static ERROR_BAD_DELEGATION_ADDRESS: &[u8] = b"No delegation contract available";
 pub static ERROR_BAD_DELEGATION_AMOUNT: &[u8] = b"Delegation amount must be at least 1 EGLD";
@@ -36,4 +30,3 @@ pub static ERROR_DELEGATION_CAP: &[u8] =
     b"Delegation cap must be higher than the total staked amount";
 pub static ERROR_ONLY_DELEGATION_ADMIN: &[u8] =
     b"Only the admin of the delegation contract can change the status";
-//pub static ERROR_NOTHING_TO_UNBOND: &[u8] = b"Nothing to unbond";
