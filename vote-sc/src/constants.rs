f6069e5c0ce3cf0ecb31d6f043c3e698bb404897
Alin Cruceat <alyn_509@yahoo.com>
2025-08-06 12:22:38 +0300

    reduce MIN_GAS_FINISH_EXEC value

--- vote-sc/src/constants.rs
+++ vote-sc/src/constants.rs
@@ -4,6 +4,6 @@ pub type GasLimit = u64;
 pub const HASH_LENGTH: usize = 32;
 pub const PROOF_LENGTH: usize = 18;
 pub const MIN_GAS_FOR_SYNC_CALL: GasLimit = 15_000_000;
-pub const MIN_GAS_FINISH_EXEC: GasLimit = 5_000_000;
+pub const MIN_GAS_FINISH_EXEC: GasLimit = 1_000_000;
 pub type ProposalId = u32;
 pub type Hash<M> = ManagedByteArray<M, HASH_LENGTH>;
